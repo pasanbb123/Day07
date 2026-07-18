a55c75b287d5aa9debe574d2111f5afcf5685d09
pasanbb123 <pasanthegrate@gmail.com>
2026-07-18 11:23:07 +0530

    guess the num

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// let num = Math.random() * 10 + 1;
+// let randomNum = Math.floor(num);
+// console.log("Secret number:", randomNum);
+
+// function btnInsertNum() {
+//     let guessedNum = parseInt(document.getElementById("guessedNum").value);
+//     console.log("User guessed:", guessedNum);
+//     let attempts = 0;
+
+//     if () {
+//         if (guessedNum === randomNum) {
+//             Swal.fire({
+//                 title: "Good job!",
+//                 text: "You clicked the button!",
+//                 icon: "success"
+//             });
+//         } else {
+//             if (guessedNum > randomNum) {
+//                 document.getElementById("test").innerText = "Guess a lower Number";
+//             } else {
+//                 document.getElementById("test").innerText = "Guess a higher Number";
+//             }
+//         }
+//     }
+// }
+    
+
+
