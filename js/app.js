27bfa7422cca1528ee4134da96bbb479cc59ba9e
pasanbb123 <pasanthegrate@gmail.com>
2026-07-18 12:43:32 +0530

    call back function

--- js/app.js
+++ js/app.js
@@ -24,5 +24,88 @@
 //     }
 // }
     
+//  Array methods -----------------------------------------------------------------
 
+// const customerList = [];
+
+
+// customerList.push(1);
+// customerList.push(2);
+// customerList.push(3);
+
+// let revAr = customerList.reverse();
+
+// console.log(revAr);
+
+
+const products = [
+    { name: "bun", inStock: true },
+    { name: "car", inStock: false },
+    { name: "bat", inStock: true },
+    { name: "van", inStock: false },
+];
+
+// final watch Line
+
+let inStockItems = products.filter(product => product.inStock == false);
+
+// let inStockItems = products.filter(
+//     function (product) {
+//         return productFilter(product)
+//     }
+// )
+
+// function productFilter(product) {
+//     return product.inStock == true;
+// }
+
+// console.log(inStockItems);
+
+// Step 01 ------------------------------
+// function getSum(num1,num2){
+//     return num1+num2;
+// }
+
+// getSum(10,20);
+
+// Step 02 ------------------------------------
+
+// let getSum = function (num1, num2){
+//     return num1+num2;
+// }
+
+// console.log(getSum(10,20));
+
+
+// Step 03 --------------------------------------
+
+// let getSum = (num1, num2) => {
+//     return num1+num2;
+// }
+
+// console.log(getSum(10,20));
+
+
+// Step 04 --------------------------------------
+
+// let sample = txtValue =>{
+//     return txtValue;
+// }
+
+// console.log(sample("Hi saman"));
+
+// Step 05 -----------------------------------------
+
+// let getSum = (num1, num2) => num1+num2;
+
+
+// console.log(getSum(10,20));
+
+
+// Step 06 --------------------------------------
+
+let sample = txtValue => txtValue;
+
+
+console.log(sample("Hi saman"));
 
